f43218f2d1a9d62489b3477b13ae1a5d011e18b5
Yixin Luo <18810541851@163.com>
2016-10-07 10:34:21 +0800

    让下载器抛出错误

--- jlitespider/src/main/java/com/github/luohaha/jlitespider/extension/Network.java
+++ jlitespider/src/main/java/com/github/luohaha/jlitespider/extension/Network.java
@@ -15,70 +15,65 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
- * author: Yixin Luo
- * 2016/3/3
+ * author: Yixin Luo 2016/3/3
  * 
- * 简单的对请求的url操作进行封装，
- * 返回对应的html字符串
+ * 简单的对请求的url操作进行封装， 返回对应的html字符串
  * 
- * **/
+ **/
 
 public class Network {
-	/*user agent*/
+	/* user agent */
 	private String agent = null;
-	/*设置cookie*/
+	/* 设置cookie */
 	private String cookie = null;
-	/*传输超时*/
+	/* 传输超时 */
 	private int timeout = 1000;
-	/*设置代理*/
+	/* 设置代理 */
 	private String proxy = null;
-	
+
 	public static Network create() {
 		return new Network();
 	}
+
 	/**
 	 * 设置下载传输参数
-	 * **/
+	 **/
 	public Network setUserAgent(String s) {
 		this.agent = s;
 		return this;
 	}
+
 	public Network setCookie(String c) {
 		this.cookie = c;
 		return this;
 	}
+
 	public Network setTimeout(int t) {
 		this.timeout = t;
 		return this;
 	}
+
 	public Network setProxy(String p) {
 		this.proxy = p;
 		return this;
 	}
+
 	/**
 	 * 下载，并返回string
-	 * **/
-	public String downloader(String url) {
+	 * @throws IOException 
+	 * @throws ClientProtocolException 
+	 **/
+	public String downloader(String url) throws ClientProtocolException, IOException {
 		String res = "";
-		try {
-			Request rq = Request.Get(url).connectTimeout(this.timeout);
-			if (this.agent != null)
-				rq = rq.userAgent(this.agent);
-			if (this.cookie != null)
-				rq = rq.addHeader("Cookie", this.cookie);
-			if (this.proxy != null)
-				rq = rq.viaProxy(this.proxy);
-			res = rq.execute().returnContent().asString();
-			
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Request rq = Request.Get(url).connectTimeout(this.timeout);
+		if (this.agent != null)
+			rq = rq.userAgent(this.agent);
+		if (this.cookie != null)
+			rq = rq.addHeader("Cookie", this.cookie);
+		if (this.proxy != null)
+			rq = rq.viaProxy(this.proxy);
+		res = rq.execute().returnContent().asString();
 		return res;
 	}
-	
-}
 
+}
